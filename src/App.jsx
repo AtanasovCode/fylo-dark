5f6de8ccd5371c531a84a4a58b1492ca5a93c655
AtanasovCode <a_ljupce@yahoo.com>
2024-09-16 19:41:35 +0200

    created mobile view of grid component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 //importing components
 import Navigation from "./components/Navigation";
 import Hero from "./components/Hero";
+import FeaturesGrid from "./components/FeaturesGrid";
 
 const App = () => {
   return (
     <div className="w-full min-h-[100vh] bg-dark-blue-main font-open p-6 flex flex-col items-center justify-center sm:p-8 md:p-10 lg:px-12 lg:py-10">
       <Navigation />
       <Hero />
+      <FeaturesGrid />
     </div>
   );
 }
--- src/components/FeaturesGrid.jsx
+++ src/components/FeaturesGrid.jsx
@@ -1,8 +1,36 @@
+//importing icons for each individual feature
+import feature1 from '../assets/images/icon-access-anywhere.svg';
+import feature2 from '../assets/images/icon-security.svg';
+import feature3 from '../assets/images/icon-collaboration.svg';
+import feature4 from '../assets/images/icon-any-file.svg';
 
+//importing reusable component
+import Feature from "./Feature";
 
 const FeaturesGrid = () => {
     return (
-        <div></div>
+        <div className="grid grid-cols-1 gap-16 items-center content-center">
+            <Feature
+                icon={feature1}
+                title="Access your files, anywhere"
+                text="The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere."
+            />
+            <Feature
+                icon={feature2}
+                title="Security you can trust"
+                text="2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files."
+            />
+            <Feature
+                icon={feature3}
+                title="Real-time collaboration"
+                text="Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required."
+            />
+            <Feature
+                icon={feature4}
+                title="Store any type of file"
+                text="Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared."
+            />
+        </div>
     );
 }
 
