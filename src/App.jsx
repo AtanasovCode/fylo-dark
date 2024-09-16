12a3b5c479b82e4f4cd918d9270ea0c0e3cad418
AtanasovCode <a_ljupce@yahoo.com>
2024-09-16 18:15:20 +0200

    created fully responsive hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 //importing components
 import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <div className="w-full min-h-[100vh] bg-dark-blue-main font-raleway p-6 sm:p-8 md:p-10 lg:px-12 lg:py-10">
+    <div className="w-full min-h-[100vh] bg-dark-blue-main font-open p-6 flex flex-col items-center justify-center sm:p-8 md:p-10 lg:px-12 lg:py-10">
       <Navigation />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,35 @@
+
+import heroImage from '../assets/images/illustration-intro.png';
+
+const Hero = () => {
+    return (
+        <div className="w-full flex flex-col items-center justify-center py-12 text-white sm:w-[80%] md:w-[70%] lg:w-[55%] lg:pt-20">
+            <div className="w-[100%] flex items-center justify-center mb-12 lg:w-[70%]">
+                <img
+                    src={heroImage}
+                    alt="illustration"
+                    className="w-full"
+                />
+            </div>
+            <div className="flex items-center justfify-center flex-col lg:w-[65%]">
+                <div className="font-bold text-2xl mb-8 text-center font-raleway lg:text-3xl">
+                    All your files in one secure location,
+                    accessible anywhere.
+                </div>
+                <div className="text-sm text-center mb-8 text-gray-300 lg:text-md">
+                    Fylo stores all your most important files in one secure location. Access them wherever
+                    you need, share and collaborate with friends family, and co-workers.
+                </div>
+                <div className="w-[80%] flex items-center justify-center md:w-[50%] lg:w-[60%]">
+                    <input 
+                        type="button"
+                        value="Get Started"
+                        className="w-full rounded-2xl text-center px-2 py-2 bg-blue text-md"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
